b93e7da99a719bacb6cfdf3f6a8d1e4c5e685ebd
Yongjun Cho <cyjun0304@deargen.me>
2023-11-25 20:38:55 +0900

    mod) remove projects

--- site.config.js
+++ site.config.js
@@ -10,7 +10,12 @@ const CONFIG = {
     github: "PurpleSand123",
     instagram: "",
   },
-  projects: [],
+  // projects: [
+  //   {
+  //     name: `morethan-log`,
+  //     href: "https://github.com/morethanmin/morethan-log",
+  //   },
+  // ],
   // blog setting (required)
   blog: {
     title: "bequite-log",
