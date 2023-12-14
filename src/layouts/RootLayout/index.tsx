8747ae61eed2efbf604a5cd9b23914b9ef93dbb2
mizzu-creations <mizzu.creations@gmail.com>
2023-12-14 20:57:20 +0900

    feat) Read Progress Bar

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -40,7 +40,7 @@ import "prismjs/components/prism-wasm.js"
 import "prismjs/components/prism-yaml.js"
 import "prismjs/components/prism-go.js"
 
-import useDebounce from "src/hooks/useDebounce"
+import useThrottle from "src/hooks/useThrottle"
 import { useRouter } from "next/router"
 
 type Props = {
@@ -52,24 +52,24 @@ const RootLayout = ({ children }: Props) => {
   const currentElementRef = useRef<HTMLDivElement | null>(null)
 
   const [blogHeight, setBlogHeight] = useState(0)
-  const [debounceScrollY, setDebounceScrollY] = useState<number>(0)
+  const [throttleScrollY, setThrottleScrollY] = useState<number>(0)
   const [scheme] = useScheme()
-
+  console.log(throttleScrollY)
   useGtagEffect()
 
-  const scrollDebounce = useDebounce(() => {
-    setDebounceScrollY(window.scrollY)
-  }, 200)
+  const scrollThrottle = useThrottle(() => {
+    setThrottleScrollY(window.scrollY)
+  }, 100)
 
   const getCurrentPercentage = () => {
     if (router.asPath === "/") return 0
 
-    let percentage = Math.ceil((debounceScrollY / blogHeight) * 100)
+    let percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
 
     if (percentage >= 90) {
       percentage = 100
     } else {
-      percentage = Math.ceil((debounceScrollY / blogHeight) * 100)
+      percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
     }
     return percentage
   }
@@ -80,9 +80,9 @@ const RootLayout = ({ children }: Props) => {
       setBlogHeight(clientHeight)
     }
 
-    window.addEventListener("scroll", scrollDebounce)
-    return () => window.removeEventListener("scroll", scrollDebounce)
-  }, [debounceScrollY])
+    window.addEventListener("scroll", scrollThrottle)
+    return () => window.removeEventListener("scroll", scrollThrottle)
+  }, [throttleScrollY])
 
   useEffect(() => {
     Prism.highlightAll()
