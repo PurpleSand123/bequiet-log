6f894692f4166f0d52afa9543c36afaa986c31c0
Yongjun Cho <cyjun0304@naver.com>
2025-11-22 05:58:35 +0900

    fix: move analytics to RootLayout

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -5,6 +5,7 @@ import Header from "./Header"
 import styled from "@emotion/styled"
 import Scripts from "src/layouts/RootLayout/Scripts"
 import useGtagEffect from "./useGtagEffect"
+import { Analytics } from "@vercel/analytics/next"
 import Prism from "prismjs/prism"
 import "prismjs/components/prism-markup-templating.js"
 import "prismjs/components/prism-markup.js"
@@ -91,6 +92,7 @@ const RootLayout = ({ children }: Props) => {
   return (
     <ThemeProvider scheme={scheme}>
       <Scripts />
+      <Analytics />
       {/* // TODO: replace react query */}
       {/* {metaConfig.type !== "Paper" && <Header />} */}
       <Header
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,16 @@ import { AppPropsWithLayout } from "../types"
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
-import { Analytics } from "@vercel/analytics/next"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
-    <>
-      <Analytics debug />
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
-        </Hydrate>
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+      </Hydrate>
+    </QueryClientProvider>
   )
 }
 
