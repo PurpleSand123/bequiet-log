79264fcd7c9869c8db1787b4b1bd871990fba640
Yongjun Cho <cyjun0304@naver.com>
2026-02-10 23:39:44 +0900

    fix: handle Notion API's nested response format change

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -36,7 +36,15 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   const detailPosts = filterPosts(posts, filter)
   const postDetail = detailPosts.find((t: any) => t.slug === slug)
-  const recordMap = await getRecordMap(postDetail?.id!)
+
+  if (!postDetail) {
+    return {
+      notFound: true,
+      revalidate: CONFIG.revalidateTime,
+    }
+  }
+
+  const recordMap = await getRecordMap(postDetail.id)
 
   await queryClient.prefetchQuery(queryKey.post(`${slug}`), () => ({
     ...postDetail,
